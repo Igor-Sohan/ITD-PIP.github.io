14e9fbf2a3e3a6cfba7c803acc3e3562d1a7494b
Igor <igorsohan481@gmail.com>
2023-02-19 18:23:31 +0300

    ITD-PIP

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const myButton = document.querySelector('#myButton');
+
+myButton.addEventListener('click', () => {
+
+});
+
+const menuIcon = document.querySelector('.header__burger');
+const menu = document.querySelector('.menu__list');
+
+menuIcon.addEventListener('click', function() {
+  menu.classList.toggle('header-active');
+
+  const spans = menuIcon.querySelectorAll('span');
+
+  if (menu.classList.contains('header-active')) {
+    spans[0].style.transform = 'rotate(45deg) translate(7px, 7px)';
+    spans[1].style.opacity = '0';
+    spans[2].style.transform = 'rotate(-45deg) translate(7px, -7px)';
+  } else {
+    spans[0].style.transform = 'none';
+    spans[1].style.opacity = '1';
+    spans[2].style.transform = 'none';
+  }
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
